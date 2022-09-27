8ce4a7cbe9e6928930051e236fcebb963dfeab88
Samuel Veloso Marçal <svm@lionx.com.br>
2022-09-27 07:31:31 -0300

    feat: changed route from "-" to "_"

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 fission spec init
 fission env create --spec --name caf-score-val-env --image nexus.sigame.com.br/fission-async:0.1.7 --builder nexus.sigame.com.br/fission-builder-3.8:0.0.1
 fission fn create --spec --name caf-score-val-fn --env caf-score-val-env --src "./func/*" --entrypoint main.caf_transaction --executortype newdeploy --maxscale 3
-fission route create --spec --name caf-score-val-rt --method POST --url /webhook/caf/score-validation --function caf-score-val-fn
+fission route create --spec --name caf-score-val-rt --method POST --url /webhook/caf/score_validation --function caf-score-val-fn
